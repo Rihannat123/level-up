1ea23ea76889a2b635f3c06a92780258a584208b
Rihannat123 <rihannat.oguns@gmail.com>
2026-03-07 18:42:37 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-import matplotlib. pyplot as plt
-x=[0,5,10,15,20,25,30]
-y1=[10,15,20,20,30,15,0]
-y2=[10,12,15,12,20,10,0]
-plt.plot(x,y1,linestyle = 'dashed', marker='D')
-plt.plot(x,y2,linestyle = 'dashed', marker='D')
-plt.title('velocity-Time Graph')
-plt.xlabel('velocity')
-plt.ylabel('Time')
-plt.xlim(5,25)
-plt.ylim(5,25)
+import matplotlib.pyplot as plt
+bloodsugarman=[113,85,90,150,149,88,93,115,135,80,77,82,120,]
+bloodsugarwomen=[67,98,89,120,132,150,84,69,89,79,120,110,100]
+type=[bloodsugarman,bloodsugarwomen]
+colors=['g','r']
+label=['men','women']
+bins=[80,100,120,150]
+plt.xlabel("Blood Sugar Range")
+plt.ylabel("Total no. of pateint")
+
+plt.hist(type, bins=bins, rwidth=0.95 , color=colors, label=label, orientation="horizontal")
+plt.title("Blood Sugar Level Chart ")
+plt.legend()
 plt.show()
