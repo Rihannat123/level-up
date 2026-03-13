52189d7b6b3a0511b4e1dc68b8e865eaa2fa32ae
Rihannat123 <rihannat.oguns@gmail.com>
2026-03-13 18:51:28 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,33 @@
-import matplotlib.pyplot as plt
-bloodsugarman=[113,85,90,150,149,88,93,115,135,80,77,82,120,]
-bloodsugarwomen=[67,98,89,120,132,150,84,69,89,79,120,110,100]
-type=[bloodsugarman,bloodsugarwomen]
-colors=['g','r']
-label=['men','women']
-bins=[80,100,120,150]
-plt.xlabel("Blood Sugar Range")
-plt.ylabel("Total no. of pateint")
+import pandas as pd
 
-plt.hist(type, bins=bins, rwidth=0.95 , color=colors, label=label, orientation="horizontal")
-plt.title("Blood Sugar Level Chart ")
-plt.legend()
+import numpy as np
+
+import seaborn as sns
+
+import matplotlib.pyplot as plt
+housedf=pd.read_csv("USA_Housing (1).csv")
+print ("/nFirst 5 Rows of Dataset")
+print(housedf.head)
+
+print ("/nlast 5 Rows of Dataset")
+print(housedf.tail())
+
+print ("/n Dataset Info")
+print(housedf.info())
+
+print ("/nColumns Names")
+print(housedf.columns)
+
+print(housedf.describe())
+
+print ("/nMissing values")
+print(housedf.isnull().sum())
+
+sns.pairplot(housedf)
+
+plt.figure(figsize=(10,6))
+sns.heatmap(housedf.corr(numeric_only=True),annot=True , cmap="coolwarm")
+
+plt.figure()
+sns.histplot(housedf["Price"], bins=30)
 plt.show()
